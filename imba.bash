8a9aea38f25b7ed2c5f4a3711d22e6cc4d3f7b8b
Yang Luo <52036519+JamesLuoYang@users.noreply.github.com>
2023-03-22 20:38:15 +1100

    Add files via upload
    AWS SageMaker auto start running notebook, detect idle state, every 5 minutes, if notebook is idle for 5 minutes, the instance will auto shut down

--- imba.bash
+++ imba.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -e
+set -ex
 ENVIRONMENT=python3
 NOTEBOOK=/home/ec2-user/SageMaker/imba-prediction.ipynb
 
@@ -13,10 +13,31 @@ nohup jupyter nbconvert --ExecutePreprocessor.timeout=-1 --ExecutePreprocessor.k
 echo "Deactivating conda environment"
 conda deactivate
 
-IDLE_TIME=300 # 5 minute
+# PARAMETERS
+IDLE_TIME=3600
 
 echo "Fetching the autostop script"
 wget https://raw.githubusercontent.com/aws-samples/amazon-sagemaker-notebook-instance-lifecycle-config-samples/master/scripts/auto-stop-idle/autostop.py
 
+
+echo "Detecting Python install with boto3 install"
+
+# Find which install has boto3 and use that to run the cron command. So will use default when available
+# Redirect stderr as it is unneeded
+CONDA_PYTHON_DIR=$(source /home/ec2-user/anaconda3/bin/activate /home/ec2-user/anaconda3/envs/JupyterSystemEnv && which python)
+if $CONDA_PYTHON_DIR -c "import boto3" 2>/dev/null; then
+    PYTHON_DIR=$CONDA_PYTHON_DIR
+elif /usr/bin/python -c "import boto3" 2>/dev/null; then
+    PYTHON_DIR='/usr/bin/python'
+else
+    # If no boto3 just quit because the script won't work
+    echo "No boto3 found in Python or Python3. Exiting..."
+    exit 1
+fi
+
+echo "Found boto3 at $PYTHON_DIR"
+
+
 echo "Starting the SageMaker autostop script in cron"
-(crontab -l 2>/dev/null; echo "*/1 * * * * /usr/bin/python $PWD/autostop.py --time $IDLE_TIME --ignore-connections") | crontab -
+
+(crontab -l 2>/dev/null; echo "*/5 * * * * $PYTHON_DIR $PWD/autostop.py --time $IDLE_TIME --ignore-connections >> /var/log/jupyter.log") | crontab -
