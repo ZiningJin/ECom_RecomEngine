ee06d8937ea7d30d1b8955b36d105d92c839b0af
Wenbo Li <liwenbo55@live.com>
2023-05-24 03:52:07 +1000

    Update "main.tf"

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,9 +1,3 @@
 provider "aws" {
   region = var.aws_region
-  ########################################
-  #   login aws on github action workflow
-  shared_credentials_files = ["/home/lawrence/.aws/credentials"]
-  ########################################
-  # access_key = "AKIAVJTYHS7LVFWANOLT"
-  # secret_key = "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"
 }
